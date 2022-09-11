59eb93f8c3f3557be812a257faab4bc605598dab
ryuji55 <rt.october.2@icloud.com>
2022-09-11 14:40:26 +0900

    Add:lintチェックとsorceryインストール

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import styled from 'styled-components'
 
-function App() {
+const Title = styled.h1`
+  text-align: center;
+  font-size: 34px;
+`
+
+const App = React.memo(function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Title>ReactPractice</Title>
+    </>
+  )
+})
 
-export default App;
+export { App }
