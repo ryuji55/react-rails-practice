61cda73511fd772146f0c053f415587f5b2300e9
ryuji55 <rt.october.2@icloud.com>
2022-09-17 21:00:06 +0900

    Add:rack-cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,20 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+ allow do
+   origins 'http://localhost:3000'
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+   resource '*',
+     headers: :any,
+     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+     credentials: true
+   end
 
-# Read more: https://github.com/cyu/rack-cors
+   #本番環境用のオリジン設定
+   allow do
+     origins 'https:<自身が設定するアプリのURL>'
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+     resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+       credentials: true
+   end
+ end
--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { RegisterPage } from './noAuth/register'
+import { RegisterPage } from './noAuth/Register'
 
 const Title = styled.h1`
   text-align: center;
