d5d16d561ea196846cfe3b0deb8a096e8cb2afe0
ryuji55 <rt.october.2@icloud.com>
2022-09-18 00:15:39 +0900

    Fix:新規登録データ登録できる確認まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
-  helper_method :login!, :current_user
+  # helper_method :login, :current_user
   
-  def login!
+  def login
     session[:user_id] = @user.id
   end
     
