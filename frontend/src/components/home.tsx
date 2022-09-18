91d2786cc418e8ca0a38ec552c6bae12f8248b16
ryuji55 <rt.october.2@icloud.com>
2022-09-18 20:39:25 +0900

    Fix:バグ修正

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { RegisterPage } from './noAuth/Register'
+import { RegisterPage } from './noAuth/register'
 
 const Title = styled.h1`
   text-align: center;
